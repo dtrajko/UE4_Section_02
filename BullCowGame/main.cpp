1f1bdd2ccdc2131ac0c61cc307657847f0c73daa
Dejan Trajkovic <dtrajko@gmail.com>
2018-11-04 03:34:10 +0100

    Save progress after Section 2, Lecture 25

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <string>
 
+void PrintIntro();
+std::string GetGuess();
+
+// the entry point for our application
 int main()
 {
-	// introduce the game
+	PrintIntro(); // introduce the game
+
+	// loop for the number of turns asking for guesses
+	std::string Guess = "";
+	constexpr unsigned short NUMBER_OF_TURNS = 5;
+	for (unsigned short count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		Guess = GetGuess(); // get a guess from the player
+
+		// repeat the guess back to them
+		std::cout << "Your guess was: '" << Guess << "'" << std::endl;
+		std::cout << std::endl;
+	}
+
+	std::cin.get();
+
+	return 0;
+}
+
+void PrintIntro()
+{
 	constexpr short WORD_LENGTH = 5;
 	std::cout << "Welcome to Bulls and Cows, a fun word game!" << std::endl;
 	std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << std::endl;
 	std::cout << std::endl;
 
-	// get a guess from the player
-	std::cout << "Enter your guess: ";
+	return;
+}
+
+std::string GetGuess()
+{
 	std::string Guess = "";
+	std::cout << "Enter your guess: ";
 	std::getline(std::cin, Guess);
-
-	// repeat the guess back to them
-	std::cout << "Your guess was: '" << Guess << "'" << std::endl;
-
-	std::cin.get();
-
-	std::cout << std::endl;
-	return 0;
+	return Guess;
 }
